558624d98bc9a109737b43401b850788a8bd1639
Stefano-Mazziotta <mazziottastefano@gmail.com>
2021-10-31 20:11:55 -0300

    [0001] Components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CompanySectionComponent } from './components/company-section/company-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CompanySectionComponent
   ],
   imports: [
     BrowserModule,
