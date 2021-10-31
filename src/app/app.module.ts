18c7e474d5fce452621a73d2deefc8582fdd1e17
Stefano-Mazziotta <mazziottastefano@gmail.com>
2021-10-31 20:32:44 -0300

    [0002] Structuration and design - navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CompanySectionComponent } from './components/company-section/company-section.component';
 
