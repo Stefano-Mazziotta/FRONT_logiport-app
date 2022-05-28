9a8d06ae8ae7951acbe075343d992c4d694578c2
Stefano-Mazziotta <mazziottastefano@gmail.com>
2022-05-28 01:48:27 -0300

    [0031] boat section - advance - search and select companies.

--- src/app/components/boat-section/boat-section.component.ts
+++ src/app/components/boat-section/boat-section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from 'src/app/interfaces/company';
+import { CompanyService } from 'src/app/services/company.service';
 
 @Component({
   selector: 'app-boat-section',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoatSectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _companyService: CompanyService
+  ) {
+  }
+
+  companies:Company[] = [];
 
   ngOnInit(): void {
+  }
+
+  public searchCompany(event: any){
+    let searchValue = event.target.value;
+    if(searchValue.length >= 3){
+
+      this._companyService.searchCompany(searchValue)
+        .subscribe({
+          next: result => {
+            this.companies = result;
+            this.processSearchResult(result);
+          },
+          error: error => {
+            console.log(error);
+          }
+        })
+        return;
+    }
+    this.companies = [];
+  }
+
+  private processSearchResult(result: Company[]){
+
+
   }
 
 }
