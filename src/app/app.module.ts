b703ff2295e3eee10423efb1940419579c4e0fd1
stefano-mazziotta <mazziottastefano@gmail.com>
2022-07-28 23:05:13 +0200

    Added timestampToDatePipe and orded file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+
 import { CompanySectionComponent } from './components/company-section/company-section.component';
 import { CompanyPopupAddComponent } from './components/company-section/company-popup-add-edit/company-popup-add-edit.component';
 import { CompanyPopupViewComponent } from './components/company-section/company-popup-view/company-popup-view.component';
+
 import { PopupConfirmDeleteComponent } from './components/popup-confirm-delete/popup-confirm-delete.component';
+
+import { CompanyLoginComponent } from './components/company-login/company-login.component';
+
 import { BoatSectionComponent } from './components/boat-section/boat-section.component';
+import { BoatPopupAddEditComponent } from './components/boat-section/boat-popup-add-edit/boat-popup-add-edit.component';
+import { BoatPopupViewComponent } from './components/boat-section/boat-popup-view/boat-popup-view.component';
+
 import { MotorAbmComponent } from './components/motor-abm/motor-abm.component';
 import { GeneratorAbmComponent } from './components/generator-abm/generator-abm.component';
-import { CompanyLoginComponent } from './components/company-login/company-login.component';
-import { BoatPopupAddEditComponent } from './components/boat-section/boat-popup-add-edit/boat-popup-add-edit.component';
+
+import { TimestampToDatePipe } from './pipes/timestampToDate.pipe';
 
 @NgModule({
   declarations: [
@@ -32,6 +41,8 @@ import { BoatPopupAddEditComponent } from './components/boat-section/boat-popup-
     GeneratorAbmComponent,
     CompanyLoginComponent,
     BoatPopupAddEditComponent,
+    BoatPopupViewComponent,
+    TimestampToDatePipe,
   ],
   imports: [
     NgxPaginationModule,
