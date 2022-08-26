bd4b1dd460096ddc2515b4b50c43c04b6add375f
stefano-mazziotta <mazziottastefano@gmail.com>
2022-08-27 00:15:39 +0200

    [dev] create nav-bottom-mobile component

---
+++ src/app/components/nav-bottom-mobile/nav-bottom-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bottom-mobile',
+  templateUrl: './nav-bottom-mobile.component.html',
+  styleUrls: ['./nav-bottom-mobile.component.scss']
+})
+export class NavBottomMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
