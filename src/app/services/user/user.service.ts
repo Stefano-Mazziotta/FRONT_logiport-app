79cbc5bcda403dc22ac9cae8df951f1f932d31c5
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-03 12:41:23 +0200

    [dev] create user.service

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private serverUrl = 'http://localhost:8080/';
+  private apiUrl = 'api/users/';
+
+  constructor(private http: HttpClient) { }
+
+  public login(userData:any):Observable<any>{
+    return this.http.post(this.serverUrl + this.apiUrl + 'login' , userData);
+  }
+
+  private existTokenObservable:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  get existToken() {
+    return this.existTokenObservable.asObservable();
+  }
+
+  set existTokenNewData(existToken:boolean){
+    this.existTokenObservable.next(existToken);
+  }
+
+
+
+}
