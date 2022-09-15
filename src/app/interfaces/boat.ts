c6f2db75a24f76b6f254414300e3b8f729dd43c4
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-15 20:49:53 +0200

    [dev] add idCompany to ISearchBoatDTO - remove commas

--- src/app/interfaces/boat.ts
+++ src/app/interfaces/boat.ts
@@ -1,47 +1,47 @@
 export interface IBoat {
     IdBoat: string;
-    IdCompany: number,
-    BoatName: string,
-    Enrollment: string,
-    DistinguishingMark: string, 
-    HullMaterial: string,
-    BoatType: string,
-    Service: string,
-    SpecificExploitation: string,
-    EnrollmentDate: number,
-    ConstructionDate: number,
-    NAT: number,
-    NAN: number,
-    Eslora: number,
-    Manga: number,
-    Puntal: number,
-    PeopleTransported: number,
-    BoatPower: string,
-    ElectricPower: string,
-    IsDeleted: number | null,
-    TimeSave: number | null,
-    TimeDeleted: number | null,
+    IdCompany: number
+    BoatName: string
+    Enrollment: string
+    DistinguishingMark: string 
+    HullMaterial: string
+    BoatType: string
+    Service: string
+    SpecificExploitation: string
+    EnrollmentDate: number
+    ConstructionDate: number
+    NAT: number
+    NAN: number
+    Eslora: number
+    Manga: number
+    Puntal: number
+    PeopleTransported: number
+    BoatPower: string
+    ElectricPower: string
+    IsDeleted: number | null
+    TimeSave: number | null
+    TimeDeleted: number | null
     TimeLastUpdate: number | null
 }
     
 export interface ICreateBoatDTO {
-    idCompany: string,
-    boatName: string,
-    enrollment: string,
-    distinguishingMark: string, 
-    hullMaterial: string,
-    boatType: string,
-    service: string,
-    specificExploitation: string,
-    enrollmentDate: number,
-    constructionDate: number,
-    nat: number,
-    nan: number,
-    eslora: number,
-    manga: number,
-    puntal: number,
-    peopleTransported: number,
-    boatPower: string,
+    idCompany: string
+    boatName: string
+    enrollment: string
+    distinguishingMark: string
+    hullMaterial: string
+    boatType: string
+    service: string
+    specificExploitation: string
+    enrollmentDate: number
+    constructionDate: number
+    nat: number
+    nan: number
+    eslora: number
+    manga: number
+    puntal: number
+    peopleTransported: number
+    boatPower: string
     electricPower: string
 }
 
@@ -50,12 +50,13 @@ export interface IUpdateBoatDTO extends ICreateBoatDTO {
 }
 
 export interface ISearchBoatDTO {
+    idCompany: string
     boatName: string
 }
 
 interface IResponse {
-    status: number,
-    type: string,
+    status: number
+    type: string
     message: string
 }
 
