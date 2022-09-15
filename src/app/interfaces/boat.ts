d4a5c06886c4f16dc1eac8786a8347908cb19390
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-15 20:33:48 +0200

    [dev] boat interface - add DTO interfaces

--- src/app/interfaces/boat.ts
+++ src/app/interfaces/boat.ts
@@ -1,25 +1,72 @@
-export interface Boat {
-    IdBoat: number | null;
-    IdCompany: number | null,
-    BoatName: string | null,
-    Enrollment: string | null,
-    DistinguishingMark: string | null, 
-    HullMaterial: string | null,
-    BoatType: string | null,
-    Service: string | null,
-    SpecificExploitation: string | null,
-    EnrollmentDate: number | null,
-    ConstructionDate: number | null,
-    NAT: number | null,
-    NAN: number | null,
-    Eslora: number | null,
-    Manga: number | null,
-    Puntal: number | null,
-    PeopleTransported: number | null,
-    BoatPower: string | null,
-    ElectricPower: string | null,
+export interface IBoat {
+    IdBoat: string;
+    IdCompany: number,
+    BoatName: string,
+    Enrollment: string,
+    DistinguishingMark: string, 
+    HullMaterial: string,
+    BoatType: string,
+    Service: string,
+    SpecificExploitation: string,
+    EnrollmentDate: number,
+    ConstructionDate: number,
+    NAT: number,
+    NAN: number,
+    Eslora: number,
+    Manga: number,
+    Puntal: number,
+    PeopleTransported: number,
+    BoatPower: string,
+    ElectricPower: string,
     IsDeleted: number | null,
     TimeSave: number | null,
     TimeDeleted: number | null,
     TimeLastUpdate: number | null
+}
+    
+export interface ICreateBoatDTO {
+    idCompany: string,
+    boatName: string,
+    enrollment: string,
+    distinguishingMark: string, 
+    hullMaterial: string,
+    boatType: string,
+    service: string,
+    specificExploitation: string,
+    enrollmentDate: number,
+    constructionDate: number,
+    nat: number,
+    nan: number,
+    eslora: number,
+    manga: number,
+    puntal: number,
+    peopleTransported: number,
+    boatPower: string,
+    electricPower: string
+}
+
+export interface IUpdateBoatDTO extends ICreateBoatDTO {
+    idBoat: string
+}
+
+export interface ISearchBoatDTO {
+    boatName: string
+}
+
+interface IResponse {
+    status: number,
+    type: string,
+    message: string
+}
+
+export interface IResponseListBoat extends IResponse {
+    data: IBoat[]
+}
+
+export interface IResponseOneBoat extends IResponse {
+    data: IBoat
+}
+
+export interface IResponseNullData extends IResponse {
+    data: null
 }
