f8b983a62e4bc7b76230af8980e2e101bf023cbc
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-19 21:26:10 +0200

    [dev] add loader to login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router';
 
 import { CookieService } from 'ngx-cookie-service';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
+import { IUserLoginDTO } from 'src/app/interfaces/user';
 
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -14,8 +16,6 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-
   constructor(
     private formBuilder: FormBuilder,
     private _cookieService: CookieService,
@@ -23,13 +23,15 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private toastr: ToastrService,
   ) {
-
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
-
   }
+
+  loginForm: FormGroup;
+  isLoading: boolean = false;
+  loginSubscription:Subscription | undefined;
 
   ngOnInit(): void {
 
@@ -39,23 +41,35 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  ngOnDestroy(): void {
+    this.loginSubscription?.unsubscribe();
+  }
+
   public onLogin(): void {
     const username = this.loginForm.get('username')?.value;
     const password = this.loginForm.get('password')?.value;
 
-    const userDTO = {
+    const user:IUserLoginDTO = {
       username,
       password
     }
-    
-    this._userService.login(userDTO).subscribe({
+
+    this.loginSubscription = this.login(user);
+  }
+
+  private login(user:IUserLoginDTO):Subscription{
+    this.isLoading = true;
+    return this._userService.login(user).subscribe({
       next: response => {
+        this.isLoading = false;
+
         const accessToken = response.data.accessToken;
         this._cookieService.set('token', accessToken);
 
         this.router.navigate(['/', 'inicio']);
       },
       error: error => {
+        this.isLoading = false;
 
         if (error.status === 401) {
           this.toastr.error('Usuario o contraseña invalido.', 'Error');
@@ -69,7 +83,6 @@ export class LoginComponent implements OnInit {
 
       }
     });
-
-  }
+  };
 
 }
