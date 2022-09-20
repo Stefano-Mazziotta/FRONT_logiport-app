ec4526439065809d0609e8bf859fb5fba9762f7b
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-20 22:54:11 +0200

    [dev]  boat selector - structure, styles and logic

--- src/app/components/boat-selector/boat-selector.component.ts
+++ src/app/components/boat-selector/boat-selector.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { IBoat, IBoatSelected, ISearchBoatDTO } from 'src/app/interfaces/boat';
+import { BoatErrorNotificationService } from 'src/app/services/boat/boat-error-notification/boat-error-notification.service';
+import { BoatService } from 'src/app/services/boat/boat.service';
 
 @Component({
   selector: 'app-boat-selector',
@@ -7,9 +12,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoatSelectorComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _boatService: BoatService,
+    private _boatErrorNotification: BoatErrorNotificationService
+  ) { }
+
+  @Output() boatSelectedEvent = new EventEmitter<IBoatSelected | null>();
+  @Input() idCompanySelected: string | null = null; 
+
+  boats: IBoat[] = [];
+
+  inputSearchValue:string = "";
+
+  isLoading: boolean = false;
+  isSelected: boolean = false;
+
+  searchBoatSubscription: Subscription | undefined; 
 
   ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    this.searchBoatSubscription?.unsubscribe();
+  }
+
+  private searchBoat(searchParams:ISearchBoatDTO): Subscription {
+    this.isLoading = true;
+    
+    return this._boatService.searchBoat(searchParams).subscribe({
+      next: result => {
+        this.boats = result.data;
+        this.isLoading = false;
+      },
+      error: error => {
+        const {status} = error;
+        this.isLoading = false;
+        this._boatErrorNotification.search(status);
+      }
+    });
+  }
+
+  public typingInputSearch(focusout: any): void {
+    const idCompany = this.idCompanySelected;
+    if(idCompany){
+      this.boats = [];
+      
+      const boatName:string = focusout.target.value;
+      const searchParams:ISearchBoatDTO = {
+        idCompany,
+        boatName
+      }
+
+
+      if (boatName.length >= 3) {
+        this.searchBoatSubscription = this.searchBoat(searchParams);
+      }
+    }
+
+  }
+
+  public selectBoat(click: any): void {
+
+    const boatName: string = click.target.innerHTML;
+    const boat = this.boats.find(elem => elem.BoatName == boatName);
+
+    if (boat) {
+      this.isSelected = true;
+      this.inputSearchValue = boat.BoatName;
+
+      const companySelected:IBoatSelected = {
+        idBoat: boat.IdBoat,
+        boatName: boat.BoatName
+      }
+
+      this.boatSelectedEvent.emit(companySelected);
+    }
+  }
+
+  public removeBoatSelected(): void {
+    
+    this.boats = [];
+    this.isSelected = false;
+    this.inputSearchValue = "";
+
+    this.boatSelectedEvent.emit(null);
   }
 
 }
