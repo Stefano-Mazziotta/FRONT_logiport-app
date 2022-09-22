58afc92f922a8627a8acdb3c5808023e0ae0ff78
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-22 20:55:47 +0200

    [dev] motor service - add logic

--- src/app/services/motor/motor.service.ts
+++ src/app/services/motor/motor.service.ts
@@ -1,9 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ICreateMotorDTO, IUpdateMotorDTO, IGetAllMotorsDTO, ISearchMotorDTO, IResponseListMotor, IResponseOneMotor, IResponseNullData } from 'src/app/interfaces/motor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MotorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private myAppUrl = 'http://localhost:8080/';
+  private myApiUrl = 'api/motors/';
+
+  public getAllMotors(params:IGetAllMotorsDTO):Observable<IResponseListMotor>{
+
+    const { idCompany, idBoat } = params;
+    const queryString = `?idCompany${idCompany}&idBoat=${idBoat}`;
+
+    return this.http.get<IResponseListMotor>(this.myAppUrl + this.myApiUrl + queryString);
+  }
+
+  public getMotorById(idMotor:string):Observable<IResponseOneMotor>{
+    return this.http.get<IResponseOneMotor>(this.myAppUrl + this.myApiUrl + idMotor );
+  }
+
+  public createMotor(motor:ICreateMotorDTO):Observable<IResponseNullData>{
+    return this.http.post<IResponseNullData>(this.myAppUrl + this.myApiUrl, motor);
+  }
+
+  public updateMotor(motor:IUpdateMotorDTO):Observable<IResponseNullData>{
+    const { idMotor } = motor;
+    return this.http.put<IResponseNullData>(this.myAppUrl + this.myApiUrl + idMotor, motor);
+  }
+
+  public deleteMotor(idMotor:string):Observable<IResponseNullData>{
+    return this.http.delete<IResponseNullData>(this.myAppUrl + this.myApiUrl + idMotor);
+  }
+
+  public searchMotor(filterParams:ISearchMotorDTO):Observable<IResponseListMotor>{
+    const { idCompany, idBoat, numberMotor } = filterParams;
+    const queryString = `?idCompany=${idCompany}&idBoat=${idBoat}&numberMotor=${numberMotor }`;
+
+    return this.http.get<IResponseListMotor>(this.myAppUrl + this.myApiUrl + queryString);
+  }
+
+
 }
