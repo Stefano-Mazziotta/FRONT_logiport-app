b0f4c9a14a0619b80768f785e3f0b4f31207a5f0
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-23 21:44:08 +0200

    [dev] service motorErrorNotification - add logic

--- src/app/services/motor/motor-error-notification/motor-error-notification.service.ts
+++ src/app/services/motor/motor-error-notification/motor-error-notification.service.ts
@@ -1,9 +1,91 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MotorErrorNotificationService {
 
-  constructor() { }
+  constructor(private toastr:ToastrService) { }
+
+  private codes = {
+    notFound: 404,
+  }
+  
+  private notificationInternalError = {
+    title: "Error!",
+    message: "Error inesperado.",
+  }
+
+  public getAll(status: number): void {
+
+
+    if (status === this.codes.notFound) {
+
+      let notification = {
+        title: "Sin resultados.",
+        message: "No existen motores.",
+      }
+
+      this.toastr.info(notification.message, notification.title);
+
+      return;
+    }
+
+    const internalError = this.notificationInternalError
+    this.toastr.error(internalError.message, internalError.title);
+  }
+
+  public getById(): void {
+    const notification = {
+      title: "Error.",
+      message: "Error al obtener el motor.",
+    }
+    this.toastr.error(notification.message, notification.title);
+  }
+
+  public create(): void {
+    let notification = {
+      title: "Error.",
+      message: "Error al crear el motor.",
+    }
+    this.toastr.error(notification.message, notification.title);
+  }
+
+  public update(): void {
+
+    let notification = {
+      title: "Error.",
+      message: "Error al editar el motor.",
+    }
+    this.toastr.error(notification.message, notification.title);
+
+  }
+
+  public delete(): void{
+    let notification = {
+      title: "Error.",
+      message: "Error al eliminar el motor.",
+    }
+    this.toastr.error(notification.message, notification.title);
+  }
+
+  public search(status: number): void{
+    if (status === this.codes.notFound) {
+
+      let notification = {
+        title: "Sin resultados.",
+        message: "No se encontraron motores.",
+      }
+
+      this.toastr.info(notification.message, notification.title);
+
+      return;
+    }
+
+    const internalError = this.notificationInternalError
+    this.toastr.error(internalError.message, internalError.title);
+  }
+
+  
 }
