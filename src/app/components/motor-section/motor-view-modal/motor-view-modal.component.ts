455e4147337cdfec186fe10df0c3848ea437e27c
stefano-mazziotta <mazziottastefano@gmail.com>
2022-09-25 17:13:43 +0200

    [dev] motor view modal - add structure, styles and logic

--- src/app/components/motor-section/motor-view-modal/motor-view-modal.component.ts
+++ src/app/components/motor-section/motor-view-modal/motor-view-modal.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { IMotor } from 'src/app/interfaces/motor';
+import { MotorErrorNotificationService } from 'src/app/services/motor/motor-error-notification/motor-error-notification.service';
+import { MotorService } from 'src/app/services/motor/motor.service';
 
 @Component({
   selector: 'app-motor-view-modal',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotorViewModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _motorService: MotorService,
+    private _motorErrorNotification: MotorErrorNotificationService
+  ) { }
+
+  @Output() closeModalEvent = new EventEmitter<boolean>();
+  @Input() idMotorClicked: string | null = null;
+
+  clickModal: boolean = false;
+  isLoading: boolean = false;
+
+  motor:IMotor = {
+    IdMotor: '',
+    IdBoat: '',
+    Quantity: '',
+    Brand: '',
+    NumberMotor: 0,
+    Model: '',
+    Type: '',
+    Power: '',
+    Location: '',
+    TimeSave: 0,
+    TimeLastUpdate: 0,
+    IsDeleted: false,
+    TimeDeleted: 0
+  }
+
+  getMotorByIdSubscription: Subscription | undefined;
 
   ngOnInit(): void {
+    const idMotor = this.idMotorClicked;
+    if(idMotor){
+      this.getMotorByIdSubscription = this.getMotorById(idMotor);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.getMotorByIdSubscription?.unsubscribe();
+  }
+
+  public closeModal(): void {
+
+    if (this.clickModal == false) {
+      this.closeModalEvent.emit();
+    }
+    this.clickModal = false;
+  }
+
+  private getMotorById(idMotor:string):Subscription{
+    this.isLoading = true;
+    return this._motorService.getMotorById(idMotor).subscribe({
+      next: response => {
+        this.isLoading = false;
+        this.motor = response.data;
+      },
+      error: error => {
+        this.isLoading = false;
+        this.closeModal();
+        this._motorErrorNotification.getById();
+      }
+    });
   }
 
 }
