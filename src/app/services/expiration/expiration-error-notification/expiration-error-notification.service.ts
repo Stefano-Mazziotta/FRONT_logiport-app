9ae3ebf6ef579c1801ceb10e9cb7688f21a8be8f
stefano-mazziotta <mazziottastefano@gmail.com>
2022-10-02 21:39:34 +0200

    [dev] expiration error notification - add logic

--- src/app/services/expiration/expiration-error-notification/expiration-error-notification.service.ts
+++ src/app/services/expiration/expiration-error-notification/expiration-error-notification.service.ts
@@ -1,9 +1,90 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpirationErrorNotificationService {
 
-  constructor() { }
+  constructor(private toastr:ToastrService) { }
+
+  private codes = {
+    notFound: 404,
+  }
+  
+  private notificationInternalError = {
+    title: "Error!",
+    message: "Error inesperado.",
+  }
+
+  public getAll(status: number): void {
+
+
+    if (status === this.codes.notFound) {
+
+      let notification = {
+        title: "Sin resultados.",
+        message: "No existen vencimientos.",
+      }
+
+      this.toastr.info(notification.message, notification.title);
+
+      return;
+    }
+
+    const internalError = this.notificationInternalError
+    this.toastr.error(internalError.message, internalError.title);
+  }
+
+  public getById(): void {
+    const notification = {
+      title: "Error.",
+      message: "Error al obtener el vencimiento.",
+    }
+    this.toastr.error(notification.message, notification.title);
+  }
+
+  public create(): void {
+    let notification = {
+      title: "Error.",
+      message: "Error al crear el vencimiento.",
+    }
+    this.toastr.error(notification.message, notification.title);
+  }
+
+  public update(): void {
+
+    let notification = {
+      title: "Error.",
+      message: "Error al editar el vencimiento.",
+    }
+    this.toastr.error(notification.message, notification.title);
+
+  }
+
+  public delete(): void{
+    let notification = {
+      title: "Error.",
+      message: "Error al eliminar el vencimiento.",
+    }
+    this.toastr.error(notification.message, notification.title);
+  }
+
+  public search(status: number): void{
+    if (status === this.codes.notFound) {
+
+      let notification = {
+        title: "Sin resultados.",
+        message: "No se encontraron vencimientos.",
+      }
+
+      this.toastr.info(notification.message, notification.title);
+
+      return;
+    }
+
+    const internalError = this.notificationInternalError
+    this.toastr.error(internalError.message, internalError.title);
+  }
+
 }
